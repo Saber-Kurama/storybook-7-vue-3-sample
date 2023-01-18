67d63542cfc03678c37d9398e2d68e6577b255f1
guochao <463561248@qq.com>
2023-01-19 00:39:54 +0800

    ✨ feat:  增加args

--- src/stories/MyButton.stories.ts
+++ src/stories/MyButton.stories.ts
@@ -1,5 +1,6 @@
 import MyButton from "../components/MyButton.vue";
 import type { Meta, StoryObj } from "@storybook/vue3";
+// import { action } from "@storybook/addon-actions";
 
 type Story = StoryObj<typeof MyButton>;
 
@@ -9,10 +10,25 @@ const meta: Meta<typeof MyButton> = {
 };
 
 export const Basic: Story = {
+  name: "基本例子",
   render: () => ({
     components: { MyButton },
     template: "<MyButton label='按钮' />",
   }),
+};
+
+export const Login: Story = {
+  name: "登录的例子",
+  render: (args: any) => ({
+    components: { MyButton },
+    setup() {
+      return { args };
+    },
+    template: "<MyButton v-bind='args' />",
+  }),
+  args: {
+    label: "登录",
+  },
 };
 
 export default meta;
